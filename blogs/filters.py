258ce41addad40dcc35f2847ca207fe755717234
ezi-code <ezrayendau2000@hotmail.com>
2025-05-27 23:30:47 +0000

    search feature implemeted

---
+++ blogs/filters.py
@@ -0,0 +1,17 @@
+"""blog filters."""
+
+from django_filters import  filterset
+from .models import Blog
+
+class BlogFilter(filterset.FilterSet):
+    """Filter for blog posts."""
+
+    class Meta:
+        model = Blog
+        fields = {
+            'title': ['icontains'],
+            'content': ['icontains'],
+            "category": ['exact'],
+            'tags': ['exact', 'icontains'],
+            'created_at': ['exact', 'year__gt', 'year__lt'],
+        }
--- blogs/urls.py
+++ blogs/urls.py
@@ -6,8 +6,10 @@ app_name = "blogs"
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="list"),
-    path("post/<slug:slug>/", views.PostDetailView.as_view(), name="detail"),
-    path("post/new/", views.PostCreateView.as_view(), name="create"),
-    path("post/<int:pk>/edit/", views.PostUpdateView.as_view(), name="update"),
-    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
+    path("posts/search/", views.SearchView.as_view(), name="search"),
+    path("posts/<slug:slug>/", views.PostDetailView.as_view(), name="detail"),
+    path("posts/new/", views.PostCreateView.as_view(), name="create"),
+    path("posts/<int:pk>/edit/", views.PostUpdateView.as_view(), name="update"),
+    path("posts/<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
+
 ]
